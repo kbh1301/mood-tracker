5b6dd10aebe18ba9bcdf2697c93a4559cc33a486
kbh1301 <kyle.hulvey@gmail.com>
2022-04-18 21:34:14 -0500

    config fix, login fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const envFetchPath = () => {
     const environment = window.location.origin.search('localhost') ? 'dev' : 'prod';
-    const fetchPath = environment != 'dev' ? 'http://localhost:3000' : 'https://kbh1301-mood-tracker-api.herokuapp.com';
+    const fetchPath = environment == 'dev' ? 'http://localhost:3000' : 'https://kbh1301-mood-tracker-api.herokuapp.com';
     return fetchPath;
 };
 
