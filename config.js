df9b2176576c4154c9e5fcaf221658bd9c2ee900
kbh1301 <kyle.hulvey@gmail.com>
2022-04-18 21:44:55 -0500

    config fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const envFetchPath = () => {
-    const environment = window.location.origin.search('localhost') ? 'dev' : 'prod';
+    const environment = window.location.origin.includes('localhost') ? 'dev' : 'prod';
     const fetchPath = environment == 'dev' ? 'http://localhost:3000' : 'https://kbh1301-mood-tracker-api.herokuapp.com';
     return fetchPath;
 };
